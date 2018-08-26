fddcee3c0511ecb350be9181d4627627feca044b
x-color <36035885+x-color@users.noreply.github.com>
2018-08-27 03:28:39 +0900

    Update descriptions

--- cmd/hlt/main.go
+++ cmd/hlt/main.go
@@ -21,18 +21,18 @@ func main() {
 	// Set application's infomations
 	app := cli.NewApp()
 	app.Name = "hlt"
-	app.Usage = "highlight texts or lines matched a given pattern in files"
-	app.Version = "0.4.1"
+	app.Usage = "highlight texts or lines in files"
+	app.Version = "0.4.2"
 	app.Author = "x-color"
 	app.HelpName = app.Name
 	app.UsageText = app.Name + " [global option] command [option]... [argument]..."
 	app.Description = strings.Join([]string{
 		app.Name,
-		"highlights texts or lines matched a given pattern in files (or standard input if no files set to arguments).",
+		"highlights texts or lines in files (or standard input if no files set to arguments).",
 	}, " ")
 	app.HideHelp = true
 
-	// It is flag of highlight commands ('word', 'line')
+	// It is flag of highlight commands
 	highlightFlags := []cli.Flag{
 		cli.StringFlag{
 			Name:        "background, b",
@@ -80,11 +80,12 @@ func main() {
 			Usage:     "highlight lines containing a text matched given pattern",
 			UsageText: app.Name + " line [option]... pattern [file]...",
 			Description: strings.Join([]string{
-				"It highlights lines containing a text matched given pattern in files (or standard input if no files set to arguments).",
-				"It highlights only charactor on by default.",
-				"Settable color is 'none','black','blue','cyan','green','magenta','red','yellow' and 0~255.",
-				"The color number(0~255) is supported by some terminals.",
-			}, " "),
+				"It highlights lines containing a text matched given pattern in files (or standard input if no files set to arguments). It highlights only charactor on by default.",
+				"",
+				"   Use -b, -c to assign highlight color. Settable color is",
+				"     - name   'none','black','blue','cyan','green','magenta','red','yellow'",
+				"     - number color number is 0~255, it is supported by some terminals",
+			}, "\n"),
 			Flags: append(highlightFlags, []cli.Flag{
 				cli.IntFlag{
 					Name:        "after",
@@ -107,11 +108,12 @@ func main() {
 			Usage:     "highlight texts matched given pattern",
 			UsageText: app.Name + " word [option]... pattern [file]...",
 			Description: strings.Join([]string{
-				"It highlights texts matched given pattern in files (or standard input if no files set to arguments).",
-				"It highlights only charactor on by default.",
-				"Settable color is 'none','black','blue','cyan','green','magenta','red','yellow' and 0~255.",
-				"The color number(0~255) is supported by some terminals.",
-			}, " "),
+				"It highlights texts matched given pattern in files (or standard input if no files set to arguments). It highlights only charactor on by default.",
+				"",
+				"   Use -b, -c to assign highlight color. Settable color is",
+				"     - name   'none','black','blue','cyan','green','magenta','red','yellow'",
+				"     - number color number is 0~255, it is supported by some terminals",
+			}, "\n"),
 			Flags:  highlightFlags,
 			Action: highlightAction(highlightText),
 		},
@@ -121,19 +123,20 @@ func main() {
 			Usage:     "highlight assigned lines",
 			UsageText: app.Name + " linen [option]... list [file]...",
 			Description: strings.Join([]string{
-				"It highlights lines assigned by list in files (or standard input if no files set to arguments).",
-				"It highlights only charactor on by default.",
-				"\nlist is made up of an integer, a type of number, a range of integers, or multiple integers ranges separated by commas.",
-				"list consists of\n",
-				"- N    line N counted from 1\n",
-				"- N~   from line N to the end of the file\n",
-				"- ~M   from the first of the file to line M\n",
-				"- N~M  from line N to line M\n",
-				"- even even-numbered lines\n",
-				"- odd  odd-numbered lines\n",
-				"Settable color is 'none','black','blue','cyan','green','magenta','red','yellow' and 0~255.",
-				"The color number(0~255) is supported by some terminals.",
-			}, " "),
+				"It highlights lines assigned by list in files (or standard input if no files set to arguments). It highlights only charactor on by default.",
+				"",
+				"   list is made up of an integer, a type of number, a range of integers, or multiple integers ranges separated by commas. list consists of",
+				"     - N    line N counted from 1",
+				"     - N~   from line N to the end of the file",
+				"     - ~M   from the first of the file to line M",
+				"     - N~M  from line N to line M",
+				"     - even even-numbered lines",
+				"     - odd  odd-numbered lines",
+				"",
+				"   Use -b, -c to assign highlight color. Settable color is",
+				"     - name   'none','black','blue','cyan','green','magenta','red','yellow'",
+				"     - number color number is 0~255, it is supported by some terminals",
+			}, "\n"),
 			Flags:  highlightFlags,
 			Action: linenAction(),
 		},
